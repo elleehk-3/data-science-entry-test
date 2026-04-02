883661b67de42d13e9468db55b5f0052012395d5
elleehk-3 <elleehk@yahoo.com>
2026-04-02 12:56:33 +0800

    Create q1.py

---
+++ q1.py
@@ -0,0 +1,19 @@
+def swap (x, y):
+# Check if x and y are numeric (i.e. int or float) 
+    numeric_types = (int, float)
+    if not (type(x) in numeric_types and type(y) in numeric_types):
+        print(-1)
+# Swap the values using arithmetic manipulation
+    x = x + y
+    y = x - y
+    x = x - y
+    # Display numerals after swapping
+    print("x =", x, ", y =", y) 
+
+# Task 2
+# Invoke the function "swap" using the following scenarios:
+# - "Apple", 10
+# - 9, 17
+
+swap ("Apple", 10)
+swap (9, 17
